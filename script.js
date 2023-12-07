2d1be0e074a2e90297fc9985c9746acc9fd7533a
Sheikh-Hanaan <149807038+Sheikh-Hanaan@users.noreply.github.com>
2023-12-07 19:48:54 +0530

    adding new game button functionality script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Selecting elements
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
@@ -12,14 +14,22 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
 
-let scores = [0, 0];
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+current0El.textContent = 0;
+current1El.textContent = 0;
+
+diceEl.classList.add("hidden");
+player0El.classList.remove("player--winner");
+player1El.classList.remove("player--winner");
+player0El.classList.add("player--active");
+player1El.classList.remove("player--active");
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -78,4 +88,22 @@ btnHold.addEventListener("click", function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener("click", function () {
+  const scores = [0, 0];
+  let currentScore = 0;
+  let activePlayer = 0;
+  let playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
 });
